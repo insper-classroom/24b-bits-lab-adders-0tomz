4a5ccd95af1503405043693aa85fb50aac9124f8
Tomaz Santos Almeida <142185957+0tomz@users.noreply.github.com>
2024-09-05 18:53:56 +0000

    fulladder com lista de componentes já

--- ula_modules.py
+++ ula_modules.py
@@ -9,24 +9,41 @@ from myhdl import *
 
 @block
 def halfAdder(a, b, soma, carry):
+    
     @always_comb
     def comb():
-        pass
+        soma.next = (a and not b) or (not a and b)
+        carry.next = (a and b)
 
     return instances()
 
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    soma1 = Signal(bool(0))
+    carry1 = Signal(bool(0))
+    carry2 = Signal(bool(0))
+
+    ha1 = halfAdder(a, b, soma1, carry1)
+    ha2 = halfAdder(soma1, c, soma, carry2)
+
     @always_comb
     def comb():
-        pass
+        carry.next = carry1 or carry2  
 
-    return instances()
+    components = [ha1, ha2, comb]
+
+    return components
 
 
 @block
-def adder2bits(x, y, soma, carry):
+def adder2bits(x, y, soma, VaiUm):
+
+    carry0 = Signal(bool(0))
+
+    ha0 = halfAdder(x[0], y[0], soma[0], carry0)
+    fa1 = fullAdder(x[1], y[1], carry0, soma[1], VaiUm)
+
     return instances()
 
 
